0e431e971be46559d9f0c49f4cadbd5d698d4fd3
chenxiaobin <chenxiaobin@meizu.com>
2017-12-29 16:53:44 +0800

    Remove webpack config

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ export default function (designWidth, rem2px) {
   div.style.display = "none";
   var head = window.document.getElementsByTagName('head')[0];
   head.appendChild(div);
-  var defaultFontSize = parseFloat(window.getComputedStyle(d, null).getPropertyValue('width'));
+  var defaultFontSize = parseFloat(window.getComputedStyle(div, null).getPropertyValue('width'));
   div.remove();
   var fontSize = window.innerWidth / designWidth * rem2px / defaultFontSize * 100 + '%';
   document.documentElement.style.fontSize = fontSize;
   var st = document.createElement('style');
-  var portrait = "@media screen and (min-width: "+window.innerWidth+"px) {html{font-size:"+ fontSize +";}}";
-  var landscape = "@media screen and (min-width: "+window.innerHeight+"px) {html{font-size:"+ fontSize +";}}"
+  var portrait = "@media screen and (min-width: " + window.innerWidth + "px) {html{font-size:" + fontSize + ";}}";
+  var landscape = "@media screen and (min-width: " + window.innerHeight + "px) {html{font-size:" + fontSize + ";}}";
   st.innerHTML = portrait + landscape;
   head.appendChild(st);
-  return defaultFontSize
+  return defaultFontSize;
 }
